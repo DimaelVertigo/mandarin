8d0582063f1a1f225ff5add15ab474ee5189b373
Klimenko Dmitry <dimael.vertigo@gmail.com>
2018-12-23 12:21:42 +0200

    fixed menu bug fixed

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -7356,4 +7356,15 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     'use strict';
 
     require('babel-polyfill');
+
+    $('.main-category').on('click', function (e) {
+      e.preventDefault();
+      $('.main-category-menu').toggleClass('main-category-menu--visible');
+      e.stopPropagation();
+    });
+    $(document).on('click', function (e) {
+      if ($(e.target).is('.main-category-menu') === false) {
+        $('.main-category-menu').removeClass('main-category-menu--visible');
+      }
+    });
   }, { "babel-polyfill": 1 }] }, {}, [327]);
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,12 +1,12 @@
 import 'babel-polyfill';
-
+var $menu = $('.main-category-menu');
 $('.main-category').on('click', function (e) {
   e.preventDefault();
-  $('.main-category-menu').toggleClass('main-category-menu--visible');
+  $menu.toggleClass('main-category-menu--visible');
   e.stopPropagation();
 });
 $(document).on('click', function (e) {
   if ($(e.target).is('.main-category-menu') === false) {
-    $('.main-category-menu').removeClass('main-category-menu--visible');
+    $menu.removeClass('main-category-menu--visible');
   }
 });
