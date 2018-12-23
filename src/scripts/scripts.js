55c8206f6c28c1a7cc2d854f9b3fc8b06f8c4006
Klimenko Dmitry <dimael.vertigo@gmail.com>
2018-12-23 12:03:55 +0200

    switch burger menu

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,12 @@
 import 'babel-polyfill';
 
-// const dropdownItems = document.querySelectorAll('.dropdown-item');
-// console.log(dropdownItems)
+$('.main-category').on('click', function (e) {
+  e.preventDefault();
+  $('.main-category-menu').toggleClass('main-category-menu--visible');
+  e.stopPropagation();
+});
+$(document).on('click', function (e) {
+  if ($(e.target).is('.main-category-menu') === false) {
+    $('.main-category-menu').removeClass('main-category-menu--visible');
+  }
+});
